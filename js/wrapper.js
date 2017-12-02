ff720ad365e2be740c9e413adc78c7d602f93168
Garcia Florian <garcia.floriian@gmail.com>
2017-12-02 23:31:51 +0100

    Add dragging and icons auto hide

--- js/wrapper.js
+++ js/wrapper.js
@@ -4,8 +4,10 @@ const containerId = 'twitch-sideplayer-container'
 const createType = 'CREATE_CHANNEL'
 const removeType = 'REMOVE_CHANNEL'
 
-// Is it possible to share the player between tabs ?
 var player = null
+var selected = null // Object of the element to be moved
+var x_pos = 0, y_pos = 0 // Stores x & y coordinates of the mouse pointer
+var x_elem = 0, y_elem = 0 // Stores top, left values (edge) of the element
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.type) {
@@ -18,7 +20,8 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 })
 
 function startVideo(channelId) {
-    if (player === null) {
+    let elem = document.getElementById(containerId)
+    if (elem === null) {
         createContainer(channelId)
     } else if(player.getChannel() != channelId) {
         clearPage()
@@ -31,6 +34,7 @@ function clearPage() {
     if (elem != null) {
         elem.parentNode.removeChild(elem)
     }
+    player = null
 }
 
 function removeContainer() {
@@ -39,16 +43,71 @@ function removeContainer() {
     chrome.runtime.sendMessage({type: removeType})
 }
 
+// Will be called when user starts dragging an element
+function drag_init(elem) {
+    // Store the object of the element which needs to be moved
+    selected = elem
+    x_elem = x_pos - selected.offsetLeft
+    y_elem = y_pos - selected.offsetTop
+}
+
+// Will be called when user is dragging an element
+function move_elem(e) {
+    x_pos = document.all ? window.event.clientX : e.pageX
+    y_pos = document.all ? window.event.clientY : e.pageY
+    if (selected !== null) {
+        // set new position
+        selected.style.left = (x_pos - x_elem) + 'px'
+        selected.style.top = (y_pos - y_elem) + 'px'
+
+        // Remove previously set right position
+        selected.style.removeProperty('right');
+        selected.style.removeProperty('bottom');
+    }
+}
+
+// Destroy the object when we are done
+function destroy() {
+    selected = null
+}
+
+// TODO : split into multiple functions for readability
 function createContainer(channelId) {
     let node = document.createElement('div')
     node.id = containerId
 
-    let close = document.createElement('p')
+    let close = document.createElement('div')
+    close.style.display = 'none'
     close.id = 'close-twitch-sideplayer'
     close.onclick = removeContainer
 
+    // Drag item
+    let move = document.createElement('div')
+    move.id = 'move-twitch-sideplayer'
+    move.style.display = 'none'
+    move.onmousedown = function () {
+        drag_init(node);
+        return false;
+    };
+    document.onmousemove = move_elem
+    document.onmouseup = destroy
+
     node.appendChild(close)
+    node.appendChild(move)
     document.body.appendChild(node)
+
+    // initial position
+    node.style.right = 0 + 'px'
+    node.style.bottom = 0 + 'px'
+
+    node.onmouseover = function(e) {
+        close.style.display = 'block'
+        move.style.display = 'block'
+    }
+    node.onmouseout = function(e) {
+        close.style.display = 'none'
+        move.style.display = 'none'
+    }
 
     let options = {
         width: 400,
@@ -61,7 +120,7 @@ function createContainer(channelId) {
 
     let embed = new Twitch.Embed(containerId, options);
     embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-        player = embed.getPlayer();
-        player.play();
+        player = embed.getPlayer()
+        player.play()
     });
 }
